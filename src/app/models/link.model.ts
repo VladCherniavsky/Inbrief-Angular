c59d31e27a0ab499f37cb6b2c82db5102b73f364
vladcherniavsky <vlad.cherniavsky@gmail.com>
2017-11-08 16:23:02 +0300

    add botstrap

--- src/app/models/link.model.ts
+++ src/app/models/link.model.ts
@@ -1,19 +1,19 @@
 import {UserModel} from './user.model';
 
 export class LinkModel {
-  _id: string = null;
-  clicks: number = 0;
-  shortLink: string = null;
-  link: string = null;
-  userId: UserModel = null;
-  tags: string [] = [];
+  _id: string;
+  clicks: number;
+  shortLink: string;
+  link: string;
+  userId: UserModel;
+  tags: string [];
 
   constructor(obj: any) {
     this._id = obj._id;
     this.clicks = obj.clicks;
     this.shortLink = obj.shortLink;
     this.link = obj.link;
-    this.userId = new UserModel(obj.userId);
+    this.userId = new UserModel(obj);
     this.tags = obj.tags;
   }
 }
